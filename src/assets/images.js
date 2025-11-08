d7c54e801996d711e4be0cf2e876fe61f40224d7
Mirjalol <nosirovmirjalol869@gmail.com>
2025-11-08 16:28:34 +0500

    add

---
+++ src/assets/images.js
@@ -0,0 +1,12 @@
+import logo from './logo.avif'
+import ipsum1 from './logoipsum7.avif'
+import ipsum2 from './logoipsum6.avif'
+import ipsum3 from './logoipsum5.avif'
+import ipsum4 from './logoipsum4.avif'
+import ipsum5 from './logoipsum3.avif'
+import ipsum6 from './logoipsum2.avif'
+import ipsum7 from './logoipsum1.avif'
+
+export const assets = [
+    logo, ipsum1, ipsum2, ipsum3, ipsum4, ipsum5, ipsum6, ipsum7
+]
